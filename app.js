ea27dd1a2fd991320c56e93bbd42b04bef2823c6
Ghulam Mustafa <157616503+G-Mustafa1@users.noreply.github.com>
2024-06-28 03:22:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,188 @@
+const obj = [
+  {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1",
+    price: 109.95,
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+  {
+    id: 2,
+    title: "Mens Casual Slim Fit T-Shirts ",
+    price: 22.3,
+    category: "men's clothing",
+    image:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    rating: { rate: 4.1, count: 259 },
+  },
+  {
+    id: 3,
+    title: "Mens Cotton Jacket",
+    price: 55.99,
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    rating: { rate: 4.7, count: 500 },
+  },
+  {
+    id: 4,
+    title: "Mens Casual Slim Fit",
+    price: 15.99,
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    rating: { rate: 2.1, count: 430 },
+  },
+  {
+    id: 5,
+    title:
+      "John Hardy Women's Legends",
+    price: 695,
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    rating: { rate: 4.6, count: 400 },
+  },
+  {
+    id: 6,
+    title: "Solid Gold Petite Micropave ",
+    price: 168,
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+    rating: { rate: 3.9, count: 70 },
+  },
+  {
+    id: 7,
+    title: "White Gold Plated Princess",
+    price: 9.99,
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+    rating: { rate: 3, count: 400 },
+  },
+  {
+    id: 8,
+    title: "Pierced Owl Rose Gold ",
+    price: 10.99,
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+    rating: { rate: 1.9, count: 100 },
+  },
+  {
+    id: 9,
+    title: "WD 2TB Elements Portable  ",
+    price: 64,
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+    rating: { rate: 3.3, count: 203 },
+  },
+  {
+    id: 10,
+    title: "SanDisk SSD PLUS 1TB Internal",
+    price: 109,
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+    rating: { rate: 2.9, count: 470 },
+  },
+  {
+    id: 11,
+    title:
+      "Silicon Power 256GB SSD 3D ",
+    price: 109,
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
+    rating: { rate: 4.8, count: 319 },
+  },
+  {
+    id: 12,
+    title:
+      "WD 4TB Gaming Drive ",
+    price: 114,
+    image: "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
+    rating: { rate: 4.8, count: 400 },
+  },
+  {
+    id: 13,
+    title: "Acer SB220Q bi 21.5 inches ",
+    price: 599,
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+    rating: { rate: 2.9, count: 250 },
+  },
+  {
+    id: 14,
+    title:
+      "Samsung 49-Inch CHG90 144Hz  ",
+    price: 999.99,
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
+    rating: { rate: 2.2, count: 140 },
+  },
+  {
+    id: 15,
+    title: "BIYLACLESEN Women's 3-in-1 ",
+    price: 56.99,
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+    rating: { rate: 2.6, count: 235 },
+  },
+  {
+    id: 16,
+    title:
+      "Lock and Love Women's ",
+    price: 29.95,
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
+    rating: { rate: 2.9, count: 340 },
+  },
+  {
+    id: 17,
+    title: "Rain Jacket Women ",
+    price: 39.99,
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+    rating: { rate: 3.8, count: 679 },
+  },
+  {
+    id: 18,
+    title: "MBJ Women's Solid  ",
+    price: 9.85,
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+    rating: { rate: 4.7, count: 130 },
+  },
+  {
+    id: 19,
+    title: "Opna Women's Short Sleeve ",
+    price: 7.95,
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+    rating: { rate: 4.5, count: 146 },
+  },
+  {
+    id: 20,
+    title: "DANVOUY Womens T Shirt ",
+    price: 12.99,
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+    rating: { rate: 3.6, count: 145 },
+  },
+];
+
+const main = document.getElementById("main");
+function cards() {
+  for (let key of obj) {
+    let div = document.createElement("div");
+    div.classList.add("item")
+
+    div.innerHTML = `
+    <div class="card" style=" width: 320px; height: 60vh; padding: 5px 10px;">
+      <center>
+        <img src="${key.image}" alt="" style="width: 250px;">
+        <div class="name"><b>Name:</b> ${key.title}</div>
+        <div class="category"><b>Category</b> : ${key.category}</div>
+        <div class="price"><b>Price:</b> $${key.price} <span>Rate: ${key.rating.rate}</span> <div>
+        <button onclick="cart1('${key.image}','${key.title}','${key.category}','${key.price}')" class="btn1 btn-outline-success">Add to cart</button>
+    </div>
+    `
+    main.appendChild(div)
+  }
+}
+cards()
